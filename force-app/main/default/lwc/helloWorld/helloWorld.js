0eec73da339f3c96af6c039be76701ee3ef4a4e2
jasonDev <jasonjfolding@gmail.com>
2023-01-15 16:54:49 +0200

    Section 6 Complete.

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,35 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    fullname = "Zero to Hero"; 
+    title = "aura";  
+
+    changeHandler(event){
+        this.title = event.target.value;
+    }
+    @track
+    address = {
+        city: 'Melbourne',
+        postcode:3008,
+        country: 'Australia'
+    }
+
+    trackHandler(event){
+        this.address = {...this.address, "city":event.target.value};
+    }
+
+    // Getter Example
+
+    users = ['John', 'Smith', 'Nik'];
+    num1 = 10;
+    num2 = 20;
+
+    get firstUser(){
+        return this.users[0];
+    }
+
+    get multiply(){
+        return this.num1 * this.num2;
+    }
+
+}
